fd8287ebb3c6ddc3c6964a0b2f9919199d585871
tookuyam <tookuyam@student.42tokyo.jp>
2024-03-16 01:57:48 +0900

    fix: test case

--- tests/src/validators/validate_numstrings_test.c
+++ tests/src/validators/validate_numstrings_test.c
@@ -9,7 +9,7 @@ TEST(validate_numstrings, basic_usage)
 	EXPECT_TRUE(validate_numstrings((char *[]){"-123"}, 1));
 	EXPECT_TRUE(validate_numstrings((char *[]){"0"}, 1));
 	EXPECT_TRUE(validate_numstrings((char *[]){"1"}, 1));
-	EXPECT_FALSE(validate_numstrings((char *[]){"-1"}, 1));
+	EXPECT_TRUE(validate_numstrings((char *[]){"-1"}, 1));
 	EXPECT_FALSE(validate_numstrings((char *[]){"-0"}, 1));
 	EXPECT_FALSE(validate_numstrings((char *[]){"a"}, 1));
 	EXPECT_FALSE(validate_numstrings((char *[]){"O"}, 1));
@@ -24,8 +24,8 @@ TEST(validate_numstrings, multi_values)
 	EXPECT_TRUE(validate_numstrings((char *[]){"-123", "123", "0"}, 3));
 	EXPECT_TRUE(validate_numstrings((char *[]){"1", "-1"}, 2));
 	EXPECT_TRUE(validate_numstrings((char *[]){"-1", "1"}, 2));
-	EXPECT_FALSE(validate_numstrings((char *[]){"123", "123abc"}, 1));
-	EXPECT_FALSE(validate_numstrings((char *[]){"123", "123abc123"}, 1));
+	EXPECT_FALSE(validate_numstrings((char *[]){"123", "123abc"}, 2));
+	EXPECT_FALSE(validate_numstrings((char *[]){"123", "123abc123"}, 2));
 }
 
 TEST(validate_numstrings, error_check)
