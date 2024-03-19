055a1534b2d7039c62bdb9d7c770516af381354a
tookuyam <tookuyam@student.42tokyo.jp>
2024-03-19 18:00:41 +0900

    fix: compress_command to compress_straight_command test

--- tests/src/compresser/compress_straight_commands_test.c
+++ tests/src/compresser/compress_straight_commands_test.c
@@ -1,9 +1,9 @@
-#include "../../submit/logics/_logics.h"
+#include "compresser.h"
 #include "command_list.h"
 #include "tyctest.h"
 #include <limits.h>
 
-TEST(internal_compress_straight_rotate, compressed_success)
+TEST(compress_straight_rotate, compressed_success)
 {
 	t_command_list	*commands;
 	t_command_list	*compressed;
@@ -12,12 +12,12 @@ TEST(internal_compress_straight_rotate, compressed_success)
 	commands->append(commands, "ra");
 	commands->append(commands, "ra");
 	commands->append(commands, "ra");
-	compressed = compress_commands(commands, 3);
+	compressed = compress_straight_rotate(commands, 3);
 	ASSERT_NE(NULL, compressed);
 	ASSERT_EQ(NULL, compressed->commands);
 }
 
-TEST(internal_compress_straight_rotate, compressed_failure)
+TEST(compress_straight_rotate, compressed_failure)
 {
 	t_command_list	*commands;
 	t_command_list	*compressed;
@@ -25,8 +25,10 @@ TEST(internal_compress_straight_rotate, compressed_failure)
 	commands = create_command_list();
 	commands->append(commands, "ra");
 	commands->append(commands, "ra");
-	compressed = compress_commands(commands, 3);
+	commands->append(commands, "sa");
+	compressed = compress_straight_rotate(commands, 3);
 	ASSERT_STREQ("ra", compressed->commands->content);
 	ASSERT_STREQ("ra", compressed->commands->next->content);
-	ASSERT_EQ(NULL, compressed->commands->next->next);
+	ASSERT_STREQ("sa", compressed->commands->next->next->content);
+	ASSERT_EQ(NULL, compressed->commands->next->next->next);
 }
