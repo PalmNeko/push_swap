1c904c9a03e41da3bf140b1a8b0d454f9b20068a
PalmNeko <tookuyam@student.42tokyo.jp>
2024-08-03 13:00:08 +0900

    add: main function

--- submit/main.c
+++ submit/main.c
@@ -3,38 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tookuyam <tookuyam@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 16:41:11 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/02/26 16:41:11 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/08/03 12:59:57 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdbool.h>
-#include "libft.h"
-#include "stack.h"
-#include "errno.h"
-#include "ft_printf.h"
-#include "validators.h"
-#include "command_list.h"
-#include "logics.h"
-#include "printers.h"
+#include "ps.h"
 
 int	main(int argc, char *argv[])
 {
-	t_stack			*input_stack;
-	t_command_list	*commands;
+	t_ps_cmdlst	*cmdlst;
 
 	if (argc == 1)
 		return (0);
-	input_stack = generate_stack_strs_to(&argv[1], argc - 1);
-	if (input_stack == NULL)
-		return (print_error());
-	commands = generate_sort_commands(input_stack);
-	destroy_stack(input_stack);
-	if (commands == NULL)
-		return (print_error());
-	print_commands(commands);
-	destroy_command_list(commands);
+	cmdlst = ps_main(argc, argv);
+	if (cmdlst == NULL)
+	{
+		ps_print_error(2);
+		return (1);
+	}
+	ps_print_cmdlst(1, cmdlst);
 	return (0);
 }
