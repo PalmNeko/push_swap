50250cc9161fdfc2c9727ad7079f3f8acfb1718e
PalmNeko <tookuyam@student.42tokyo.jp>
2024-08-03 13:41:24 +0900

    fix: sort logic

--- submit/calculators/ps_sort_for_three.c
+++ submit/calculators/ps_sort_for_three.c
@@ -27,9 +27,9 @@ int	ps_sort_for_three(t_push_swap *ps)
 	second = *(int *)ft_lstat(ps->stack_a->top, 1)->content;
 	third = *(int *)ft_lstat(ps->stack_a->top, 2)->content;
 	max = ft_max(first, ft_max(second, third));
-	if (max == first && ps_rra(ps) == -1)
+	if (max == first && ps_ra(ps) == -1)
 		return (-1);
-	if (max == second && ps_ra(ps) == -1)
+	if (max == second && ps_rra(ps) == -1)
 		return (-1);
 	first = *(int *)ft_lstat(ps->stack_a->top, 0)->content;
 	second = *(int *)ft_lstat(ps->stack_a->top, 1)->content;
