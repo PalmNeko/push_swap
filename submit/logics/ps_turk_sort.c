6f6bb7d8096f5c1beeed7637bc998e206f2ce36a
PalmNeko <tookuyam@student.42tokyo.jp>
2024-08-04 13:01:21 +0900

    chore: refactor with split init, finalize and logic

--- submit/logics/ps_turk_sort.c
+++ submit/logics/ps_turk_sort.c
@@ -14,28 +14,55 @@
 #include "libft.h"
 #include <stdlib.h>
 
+int			ps_init_turk_sort(const int *values, int size, t_push_swap **ps);
+void		ps_finalize_turk_sort(t_push_swap **ps);
+t_ps_cmdlst	*ps_solve_with_turk_sort(t_push_swap *ps);
+
 t_ps_cmdlst	*ps_turk_sort(int *values, int size)
 {
 	t_push_swap	*ps;
 	t_ps_cmdlst	*cmdlst;
+
+	if (ps_init_turk_sort(values, size, &ps) == -1)
+		return (NULL);
+	cmdlst = ps_solve_with_turk_sort(ps);
+	ps_finalize_turk_sort(&ps);
+	return (cmdlst);
+}
+
+t_ps_cmdlst	*ps_solve_with_turk_sort(t_push_swap *ps)
+{
+	t_ps_cmdlst	*cmdlst;
+
+	if (ft_lstsize(ps->stack_a->top) == 3 && ps_sort_for_three(ps) == -1)
+		return (NULL);
+	cmdlst = ps->cmdlst;
+	ps->cmdlst = NULL;
+	return (cmdlst);
+}
+
+int	ps_init_turk_sort(const int *values, int size, t_push_swap **ps)
+{
 	int			*copy;
 
 	copy = (int *)malloc(sizeof(int) * size);
 	if (copy == NULL)
-		return (NULL);
+		return (-1);
 	ft_memcpy(copy, values, sizeof(int) * size);
 	if (ps_validate_no_duplicates(copy, size) == false)
-		return (free(copy), NULL);
+		return (free(copy), -1);
 	if (ps_renumber(copy, size) == -1)
-		return (free(copy), NULL);
-	ps = ps_new_ps(copy, size);
+		return (free(copy), -1);
+	*ps = ps_new_ps(copy, size);
 	free(copy);
-	if (ps == NULL)
-		return (NULL);
-	if (ft_lstsize(ps->stack_a->top) == 3 && ps_sort_for_three(ps) == -1)
-		return (ps_destroy_ps(ps), NULL);
-	cmdlst = ps->cmdlst;
-	ps->cmdlst = NULL;
-	ps_destroy_ps(ps);
-	return (cmdlst);
+	if (*ps == NULL)
+		return (-1);
+	return (0);
+}
+
+void	ps_finalize_turk_sort(t_push_swap **ps)
+{
+	ps_destroy_ps(*ps);
+	*ps = NULL;
+	return ;
 }
