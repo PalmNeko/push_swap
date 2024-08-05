d11593a28d0bfbc3a05a90c755289b42662868db
PalmNeko <tookuyam@student.42tokyo.jp>
2024-08-05 14:22:31 +0900

    fix: leak

--- submit/controllers/ps_pa.c
+++ submit/controllers/ps_pa.c
@@ -11,15 +11,19 @@
 /* ************************************************************************** */
 
 #include "ps.h"
+#include <stdlib.h>
 
 int	ps_pa(t_push_swap *ps)
 {
 	t_list	*top_b;
+	int		value;
 
 	top_b = ps_pop_stack(ps->stack_b);
 	if (top_b == NULL)
 		return (0);
-	if (ps_push_stack(ps->stack_a, *(int *)top_b->content) == -1)
+	value = *(int *)top_b->content;
+	ft_lstdelone(top_b, free);
+	if (ps_push_stack(ps->stack_a, value) == -1)
 		return (-1);
 	if (ps_append_cmdlst(ps->cmdlst, "pa") == -1)
 		return (-1);
--- submit/controllers/ps_pb.c
+++ submit/controllers/ps_pb.c
@@ -11,15 +11,19 @@
 /* ************************************************************************** */
 
 #include "ps.h"
+#include <stdlib.h>
 
 int	ps_pb(t_push_swap *ps)
 {
 	t_list	*top_a;
+	int		value;
 
 	top_a = ps_pop_stack(ps->stack_a);
 	if (top_a == NULL)
 		return (0);
-	if (ps_push_stack(ps->stack_b, *(int *)top_a->content) == -1)
+	value = *(int *)top_a->content;
+	ft_lstdelone(top_a, free);
+	if (ps_push_stack(ps->stack_b, value) == -1)
 		return (-1);
 	if (ps_append_cmdlst(ps->cmdlst, "pb") == -1)
 		return (-1);
