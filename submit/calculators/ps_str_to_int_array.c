72397a5d945b730ae19ba309c2234646de3c5e93
PalmNeko <tookuyam@student.42tokyo.jp>
2024-08-05 13:13:11 +0900

    fix: overflow checker

--- submit/calculators/ps_str_to_int_array.c
+++ submit/calculators/ps_str_to_int_array.c
@@ -12,12 +12,15 @@
 
 #include "libft.h"
 #include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
 
 int	*ps_str_to_int_array(const char *strs[], int size)
 {
 	int		*array;
 	int		index;
 	char	*endptr;
+	long	tmp;
 
 	array = (int *)malloc(sizeof(int) * size);
 	if (array == NULL)
@@ -25,9 +28,12 @@ int	*ps_str_to_int_array(const char *strs[], int size)
 	index = 0;
 	while (index < size)
 	{
-		array[index] = (int)ft_strtol(strs[index], &endptr, 10);
-		if (strs[index][0] == '\0' || *endptr != '\0')
+		errno = 0;
+		tmp = ft_strtol(strs[index], &endptr, 10);
+		if (strs[index][0] == '\0' || *endptr != '\0' || errno != 0
+			|| tmp < INT_MIN || tmp > INT_MAX)
 			return (free(array), NULL);
+		array[index] = (int)tmp;
 		index++;
 	}
 	return (array);
