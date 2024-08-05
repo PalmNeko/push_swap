f6c826c645343d298d508bf8dc150ebc8d587122
PalmNeko <tookuyam@student.42tokyo.jp>
2024-08-05 10:42:49 +0900

    refactor: replaced redundant code to a function

--- submit/logics/ps_rotate_a_to_asc.c
+++ submit/logics/ps_rotate_a_to_asc.c
@@ -17,17 +17,7 @@ int	ps_rotate_a_to_asc(t_push_swap *ps)
 	int			min_ra_cnt;
 
 	min_ra_cnt = ps_get_insert_pos_asc(ps->stack_a, -1);
-	while (min_ra_cnt > 0)
-	{
-		if (ps_ra(ps) == -1)
-			return (-1);
-		min_ra_cnt--;
-	}
-	while (min_ra_cnt < 0)
-	{
-		if (ps_rra(ps) == -1)
-			return (-1);
-		min_ra_cnt++;
-	}
+	if (ps_rotate_two_way(ps, ps_ra, ps_rra, min_ra_cnt) == -1)
+		return (-1);
 	return (0);
 }
