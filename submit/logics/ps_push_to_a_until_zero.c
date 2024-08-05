c1644745395e7d0cf5b2651d555ddccb3fbee0f9
PalmNeko <tookuyam@student.42tokyo.jp>
2024-08-05 10:57:29 +0900

    refactor: renamed variable name

--- submit/logics/ps_push_to_a_until_zero.c
+++ submit/logics/ps_push_to_a_until_zero.c
@@ -14,14 +14,14 @@
 
 int	ps_push_to_a_until_zero(t_push_swap *ps)
 {
-	int			min_ra_cnt;
+	int	ra_cnt;
 
 	while (ft_lstsize(ps->stack_b->top) > 0)
 	{
 		// スタックBの先頭と、スタックAの一番下を比較して、スタックAの方が大きければ、回転。
 		ps_print_ps(2, ps);
-		min_ra_cnt = ps_get_insert_pos_asc(ps->stack_a, *(int *)ps->stack_b->top->content);
-		if (ps_rotate_two_way(ps, ps_ra, ps_rra, min_ra_cnt) == -1)
+		ra_cnt = ps_get_insert_pos_asc(ps->stack_a, *(int *)ps->stack_b->top->content);
+		if (ps_rotate_two_way(ps, ps_ra, ps_rra, ra_cnt) == -1)
 			return (-1);
 		if (ps_pa(ps) == -1)
 			return (-1);
--- submit/logics/ps_rotate_a_to_asc.c
+++ submit/logics/ps_rotate_a_to_asc.c
@@ -14,10 +14,10 @@
 
 int	ps_rotate_a_to_asc(t_push_swap *ps)
 {
-	int			min_ra_cnt;
+	int	ra_cnt;
 
-	min_ra_cnt = ps_get_insert_pos_asc(ps->stack_a, -1);
-	if (ps_rotate_two_way(ps, ps_ra, ps_rra, min_ra_cnt) == -1)
+	ra_cnt = ps_get_insert_pos_asc(ps->stack_a, -1);
+	if (ps_rotate_two_way(ps, ps_ra, ps_rra, ra_cnt) == -1)
 		return (-1);
 	return (0);
 }
