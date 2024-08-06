031fbda02cd9977ed1253e2ed2ff13a237c4f21a
PalmNeko <tookuyam@student.42tokyo.jp>
2024-08-06 16:26:19 +0900

    add: comment

--- submit/ps.h
+++ submit/ps.h
@@ -15,6 +15,7 @@
 
 # include "ps_types.h"
 
+/** stack */
 void		ps_destroy_stack(t_ps_stack *stack);
 t_list		*ps_get_stack(const t_ps_stack *stack, int n);
 t_ps_stack	*ps_new_stack(void);
@@ -22,11 +23,13 @@ t_list		*ps_pop_stack(t_ps_stack *stack);
 int			ps_push_stack(t_ps_stack *stack, int value);
 void		ps_swap_stack(const	t_ps_stack *stack);
 
+/** command */
 int			ps_append_cmdlst(t_ps_cmdlst *cmdlst, const char *command);
 void		ps_destroy_cmdlst(t_ps_cmdlst *cmdlst);
 t_list		*ps_get_cmdlst(t_ps_cmdlst *cmdlst, int n);
 t_ps_cmdlst	*ps_new_cmdlst(void);
 
+/** push swap */
 void		ps_destroy_ps(t_push_swap *ps);
 t_push_swap	*ps_new_ps(const int *values, int size);
 int			ps_pa(t_push_swap *ps);
@@ -41,9 +44,11 @@ int			ps_sa(t_push_swap *ps);
 int			ps_sb(t_push_swap *ps);
 int			ps_ss(t_push_swap *ps);
 
+/** validate */
 bool		ps_validate_no_duplicates(int *values, size_t len);
 bool		ps_validate_is_sorted(t_push_swap *ps);
 
+/** calculator */
 int			ps_renumber(int *values, int size);
 int			*ps_str_to_int_array(const char *strs[], int size);
 int			ps_sort_for_three(t_push_swap *ps);
@@ -54,6 +59,7 @@ int			ps_get_max_value(t_ps_stack *stack);
 int			ps_convert_min_pos(int pos, int size);
 int			ps_calc_rotate_cost(int rotate_a, int rotate_b);
 
+/** logic */
 int			ps_run_to_times(t_push_swap *ps, int (*f)(t_push_swap *), int cnt);
 int			ps_rotate_two_stack(t_push_swap *ps, int rotate_a, int rotate_b);
 int			ps_rotate_two_way(t_push_swap *ps, t_cmd fr, t_cmd frr, int cnt);
@@ -68,6 +74,7 @@ void		ps_print_error(int fd);
 void		ps_print_ps(int fd, t_push_swap *ps);
 t_ps_cmdlst	*ps_push_swap_sort(int *values, int size);
 
+/** algorithm */
 int			ps_turk_sort(t_push_swap *ps);
 int			ps_quick_sort(t_push_swap *ps);
 
