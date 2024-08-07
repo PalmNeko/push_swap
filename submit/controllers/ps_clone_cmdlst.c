13bc302ed9e0d4dd301350cfa7b13491e32312bc
PalmNeko <tookuyam@student.42tokyo.jp>
2024-08-07 18:12:38 +0900

    fix: clone cmdlst function

--- submit/controllers/ps_clone_cmdlst.c
+++ submit/controllers/ps_clone_cmdlst.c
@@ -22,7 +22,7 @@ t_ps_cmdlst	*ps_clone_cmdlst(t_ps_cmdlst *cmdlst)
 	if (cloned_cmdlst == NULL)
 		return (NULL);
 	cloned_lst = ft_lstmap(cmdlst->top, (void *(*)(void *))ft_strdup, free);
-	if (cloned_lst == NULL)
+	if (cmdlst->top != NULL && cloned_lst == NULL)
 		return (ps_destroy_cmdlst(cloned_cmdlst), NULL);
 	cloned_cmdlst->top = cloned_lst;
 	return (cloned_cmdlst);
