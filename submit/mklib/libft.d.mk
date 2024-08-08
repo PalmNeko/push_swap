24d9ea4bad707010295e99490dc241f55cc3e97d
PalmNeko <nitunamoni13579@gmail.com>
2024-08-08 19:06:43 +0900

    delete: clean and fclean rule

--- submit/mklib/libft.d.mk
+++ submit/mklib/libft.d.mk
@@ -6,7 +6,7 @@
 #    By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/07/22 12:28:22 by tookuyam          #+#    #+#              #
-#    Updated: 2024/07/30 14:23:53 by tookuyam         ###   ########.fr        #
+#    Updated: 2024/08/08 19:05:29 by tookuyam         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,12 +21,3 @@ $(LIBFT_DIR)/$(LIBFT): $(LIBFT_DIR)
 
 $(LIBFT_DIR):
 	git clone https://github.com/PalmNeko/Libft
-
-clean_libft:
-	make -C "$(LIBFT_DIR)" clean
-
-fclean_libft:
-	make -C "$(LIBFT_DIR)" fclean
-
-clean: clean_libft
-fclean: fclean_libft
