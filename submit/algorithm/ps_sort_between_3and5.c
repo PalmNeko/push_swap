249e5125bcd116d7d81a6e41f67ad9930865fe87
PalmNeko <nitunamoni13579@gmail.com>
2024-08-09 14:37:31 +0900

    add: function to description

--- submit/algorithm/ps_sort_between_3and5.c
+++ submit/algorithm/ps_sort_between_3and5.c
@@ -14,30 +14,43 @@
 
 int	is_swap(t_push_swap *ps);
 int	ps_push_and_rotate(t_push_swap *ps);
+int ps_rotate_stack_a_when_min_values_placed_bottom(t_push_swap *ps);
+int	ps_swap_stack_a_to_take_min_cost(t_push_swap *ps);
 
 int	ps_sort_between_3and5(t_push_swap *ps)
+{
+	if (ps_rotate_stack_a_when_min_values_placed_bottom(ps) == -1)
+		return (-1);
+	if (ps_push_target_until(ps, PS_TB, 3) == -1)
+		return (-1);
+	if (ps_sort_to_serial_number_only_top_three(ps, PS_TA) == -1)
+		return (-1);
+	if (ps_swap_stack_a_to_take_min_cost(ps) == -1)
+		return (-1);
+	if (ps_push_to_a_until_zero(ps) == -1)
+		return (-1);
+	if (ps_rotate_a_to_asc(ps) == -1)
+		return (-1);
+	return (0);
+}
+
+int ps_rotate_stack_a_when_min_values_placed_bottom(t_push_swap *ps)
 {
 	if (ps_at(ps->stack_a, 3) <= 1 && ps_at(ps->stack_a, 4) <= 1)
 	{
 		if (ps_run_to_times(ps, ps_rra, 2) == -2)
 			return (-1);
 	}
-	while (ps->stack_a->size > 3)
-	{
-		if (ps_pb(ps) == -1)
-			return (-1);
-	}
-	if (ps_sort_to_serial_number_only_top_three(ps, PS_TA) == -1)
-		return (-1);
+	return (0);
+}
+
+int	ps_swap_stack_a_to_take_min_cost(t_push_swap *ps)
+{
 	if (is_swap(ps) == 1)
 	{
 		if (ps_sb(ps) == -1)
 			return (-1);
 	}
-	if (ps_push_to_a_until_zero(ps) == -1)
-		return (-1);
-	if (ps_rotate_a_to_asc(ps) == -1)
-		return (-1);
 	return (0);
 }
 
